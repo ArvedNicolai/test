c2d9c88b25e35b7ab51a895b5bdeef68861ca41e
ArvedArv <arvdt@gmx.de>
2019-10-22 20:22:14 +0200

    Ini
    tial

--- Gitta/src/mainclass.java
+++ Gitta/src/mainclass.java
@@ -3,7 +3,7 @@ public class mainclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("Hallo.");
+		System.out.print("Hallo2.");
 		System.out.print("Hallo.");
 		System.out.print("Hallo.");
 
