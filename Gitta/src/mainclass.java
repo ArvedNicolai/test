7fad840cef38a82cf9770a03e5794afef440972c
ArvedArv <arvdt@gmx.de>
2019-10-22 20:20:10 +0200

    Initial Commit
    Initial

---
+++ Gitta/src/mainclass.java
@@ -0,0 +1,14 @@
+public class mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hallo.");
+		System.out.print("Hallo.");
+		System.out.print("Hallo.");
+
+
+		
+	}
+
+}
